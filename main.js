648a1afe11d5f8894b5ea1b68052ace0d5984b08
Petr Korolev <sky4winder@gmail.com>
2020-06-04 15:17:21 +0700

    add files

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+require('dotenv').config()
+
 const { Telegraf } = require('telegraf')
 
+console.log(process.env.BOT_TOKEN)
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.action
 bot.launch()
+ 
